0f3e180704ca7537f63e8a126bcb2c8c9b8e5b9d
Huang Kai <hk@ciphergateway.com>
2017-08-11 15:36:26 +0800

    IfStmnt ParserRunner

--- src/main/java/runner/ParserRunner.java
+++ src/main/java/runner/ParserRunner.java
@@ -4,10 +4,19 @@
  */
 package runner;
 
+import ast.ASTree;
+import lexer.Lexer;
 import lexer.ParseException;
+import lexer.Token;
+import parser.BasicParser;
 
 public class ParserRunner {
-  public static void main(String[] args) throws ParseException{
-
+  public static void main(String[] args) throws ParseException {
+    Lexer l = new Lexer(new CodeDialog());
+    BasicParser bp = new BasicParser();
+    while (l.peek(0) != Token.EOF) {
+      ASTree ast = bp.parser(l);
+      System.out.println("=> " + ast.toString());
+    }
   }
 }
